731bf282b0203fc02cf6cdda0ae4cbd13df527d4
mind_control <mind_control@mail.ru>
2020-06-19 14:19:32 +0300

    Initial Commit

---
+++ src/test/java/ru/netology/generator/DataGenerator.java
@@ -0,0 +1,34 @@
+package ru.netology.generator;
+
+import com.github.javafaker.Faker;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.Locale;
+
+public class DataGenerator {
+    private Faker faker;
+    private LocalDate today = LocalDate.now();
+    private DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd.MM.yyyy");
+
+    public String forwardDate(int plusDays) {
+        LocalDate newDate = today.plusDays(plusDays);
+        return formatter.format(newDate);
+    }
+
+    public String makeCity() {
+        String[] myCityList = new String[]{"Абакан", "Анадырь", "Архангельск", "Астрахань", "Барнаул", "Владикавказ", "Горно-Алтайск", "Йошкар-Ола", "Казань", "Калининград", "Калуга", "Краснодар", "Магас", "Махачкала", "Нарьян-Мар", "Салехард", "Самара", "Саранск", "Саратов", "Хабаровск", "Ханты-Мансийск", "Южно-Сахалинск", "Великий Новгород", "Владивосток", "Владимир", "Вологда", "Рязань", "Биробиджан", "Чебоксары", "Москва", "Санкт-Петербург", "Ульяновск", "Симферополь", "Ростов-на-Дону"};
+        int city = (int) Math.floor(Math.random() * myCityList.length);
+        return myCityList[city];
+    }
+
+    public String makeName() {
+        faker = new Faker(new Locale("ru"));
+        return faker.name().fullName();
+    }
+
+    public String makePhone() {
+        faker = new Faker(new Locale("ru"));
+        return faker.phoneNumber().phoneNumber();
+    }
+}
