cf4a0763d5215706e0647c2aeff4682ba34f02b7
mind_control <mind_control@mail.ru>
2020-06-21 01:27:50 +0300

    Rename Methods

--- src/test/java/ru/netology/order/OrderCardDeliveryTest.java
+++ src/test/java/ru/netology/order/OrderCardDeliveryTest.java
@@ -13,6 +13,7 @@ import static com.codeborne.selenide.Selenide.open;
 
 public class OrderCardDeliveryTest {
     DataGenerator dataGenerator = new DataGenerator();
+
     @BeforeEach
     void Setup() {
         open("http://localhost:9999");
